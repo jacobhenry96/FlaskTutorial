e82d2c4e77e2a4c7c908dd4ecd0016f4b8d9c572
jacobhenry96 <jacobkhenry2@gmail.com>
2025-01-09 09:56:15 -0600

    Day 2 of lesson 4

--- 4fourthapp/app4.py
+++ 4fourthapp/app4.py
@@ -1,17 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
-    elif request.method == "POST":
-        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'jacob' and password == "password":
+            return 'Success'
+        else:
+            return 'Failure'
+
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    file  = request.files['file']
+
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
+        df = pd.read_excel(file)
+        return df.to_html()
+
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+    df = pd.read_excel(file)
+
+    response = Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content-Disposition': "attachment; filename=result.csv"
+        }
+    )
+
+    return ""
+
+@app.route('/convert_csv_two', methods=['POST'])
+def convert_csv_two():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
