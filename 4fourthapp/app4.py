bd7e593cb13760e3ae5551924053aefd4664f03a
jacobhenry96 <jacobkhenry2@gmail.com>
2025-01-10 10:44:24 -0600

    complete 4th lesson

--- 4fourthapp/app4.py
+++ 4fourthapp/app4.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, Response
+import os
+import uuid
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify
 import pandas as pd
 
 app = Flask(__name__, template_folder='templates')
@@ -48,7 +50,27 @@ def convert_csv_two():
 
     df = pd.read_excel(file)
 
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+    
+    filename = f'{uuid.uuid4()}.csv'
+    df.to_csv(os.path.join('downloads', filename))
 
+    return render_template('download.html', filename=filename)
+
+@app.route('/download')
+def download(filename):
+    return send_from_directory('downloads', filename, download_name='result.csv')
+
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
+
+    with open('file.txt', 'w') as f:
+        f.write(f'{greeting}, {name}')
+    
+    return jsonify({'message': 'Successfully written!'})
 
 
 if __name__ == '__main__':
